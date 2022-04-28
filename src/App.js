53f6ef39cffce952db94c4afd95ad149b83dd1e8
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-04-28 07:05:29 +0300

    adds template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { FilterPanel } from './components/FilterPanel';
+import { JobList } from './components/JobList';
+import { TheHeader } from './components/TheHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <TheHeader />
+     <div className='container'>
+      <FilterPanel />
+      <JobList />
+     </div>
+    </>
   );
 }
 
