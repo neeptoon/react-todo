4aef8f4bdf5607d4873563a90ce2417ceb34b92e
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-05-05 13:55:48 +0300

    add redux-persist

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,4 +1,4 @@
-import { JobPosition } from './JobPosition';
+import {JobPosition} from './JobPosition';
 import {useSelector, useDispatch} from "react-redux";
 import {selectVisiblePositions} from "../store/positions/position-selector";
 import {addFilter} from "../store/filters/filter-action";
@@ -7,26 +7,26 @@ import {selectFilters} from "../store/filters/filter-selector";
 
 const JobList = () => {
 
-  const currentFilters = useSelector(selectFilters);
-  const positions = useSelector((state) => selectVisiblePositions(state, currentFilters));
+	const currentFilters = useSelector(selectFilters);
+	const positions = useSelector((state) => selectVisiblePositions(state, currentFilters));
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const handleAddFilter = (filter) => {
-    dispatch(addFilter(filter))
-  }
+	const handleAddFilter = (filter) => {
+		dispatch(addFilter(filter))
+	}
 
-  return (
-    <div className='job-list'>
-      {positions.map(item => (
-        <JobPosition
-            key={item.id}
-            {...item}
-            handleAddFilter={handleAddFilter}
-        />
-      ))}
-    </div>
-  )
+	return (
+		<div className="job-list">
+			{positions.map(item => (
+				<JobPosition
+					key={item.id}
+					{...item}
+					handleAddFilter={handleAddFilter}
+				/>
+			))}
+		</div>
+	)
 }
 
 export {JobList};
